9ca71867cdd388d9b4dcd5c8dfd540e602a8f064
NataManatu <vip.aleksa78@mail.ru>
2022-08-04 15:59:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let cell = document.getElementById("cell")
+let ticTacToe = document.getElementsByClassName(" TicTacToe")
+
+function one(){
+    alert("клик")
+}
+
+for (let index = 0; index <ticTacToe.length; index++) {
+     ticTacToe[index].addEventListener("click",one)
+}
