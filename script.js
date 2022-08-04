af69ce90f86d04b26f5a7c4a3e1034251d6f0506
NataManatu <vip.aleksa78@mail.ru>
2022-08-04 16:55:42 +0300

    TicTacToe

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let cell = document.querySelectorAll("#cell")
 let ticTacToe = document.getElementsByClassName(" TicTacToe")
+let currentPlayer = "q"
 
-function place(){
-    alert("клик")
+for (let index = 0; index < cell.length; index++) {
+    cell[index].addEventListener("click", place)
 }
 
-for (let index = 0; index <cell.length; index++) {
-     cell[index].addEventListener("click",place)
+function place() {
+   cell.innerHTML = currentPlayer;
+    if (currentPlayer == "q") {
+        currentPlayer = "s";
+    }
+    else {
+        currentPlayer = "q";
+    }
+    alert(currentPlayer)
 }
+
+
