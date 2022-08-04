af25e52ca44658d4f04aced4fec127122023e164
NataManatu <vip.aleksa78@mail.ru>
2022-08-04 23:57:03 +0300

    TicTacToe

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 let cell = document.querySelectorAll("#cell")
 let ticTacToe = document.getElementsByClassName(" TicTacToe")
 let currentPlayer = "O"
-
+let won = false;
 
 // for (let index = 0; index < cell.length; index++) {
 //     cell[index].addEventListener("click", place)
 // }
 
 function place(box) {
-
-    if (box.innerHTML != "") return;
-    box.innerHTML = currentPlayer;
+    if (box.innerText != "" || won) return;
+    box.innerText = currentPlayer;
     currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
-    console.log(currentPlayer)
+    checkGameBoard()
 }
-let first = "O";
-let second = "X";
-let third = "O";
-if (first == second && first == third){
-    alert("победитель");
+
+function checkGameBoard() {
+    for (let i = 0; i <= 2; i++) {
+        let first = document.getElementById("0_" + i).innerText
+        let second = document.getElementById("1_" + i).innerText
+        let third = document.getElementById("2_" + i).innerText
+        third.innerText
+        if (first == "") continue
+        if (first == second && first == third) {
+            alert("победитель")
+        }
+    }
+    for (let i = 0; i <= 2; i++) {
+        let first = document.getElementById(i + "_0").innerText
+        let second = document.getElementById(i + "_1").innerText
+        let third = document.getElementById(i + "_2").innerText
+        third.innerText
+        if (first == "") continue
+        if (first == second && first == third) {
+            alert("победитель")
+
+        }
+    }
+    let firstD1 = document.getElementById("0_0").innerText
+    let secondD1 = document.getElementById("1_1").innerText
+    let thirdD1 = document.getElementById("2_2").innerText
+    if (firstD1 != "" && firstD1 == secondD1 && firstD1 == thirdD1) {
+        alert("победитель")
+    }
+    let firstD2 = document.getElementById("0_2").innerText
+    let secondD2 = document.getElementById("1_1").innerText
+    let thirdD2 = document.getElementById("2_0").innerText
+    if (firstD2 != "" && firstD2 == secondD2 && firstD2 == thirdD2) {
+        alert("победитель")
+    }
 }
