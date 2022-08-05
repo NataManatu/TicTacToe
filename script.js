21070e6e3b4e897a3b2dfbf4d84ee18d0551f02d
NataManatu <vip.aleksa78@mail.ru>
2022-08-05 21:12:43 +0300

    TicTacToe

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let currentPlayer = "X"
 // }
 
 function place(box) {
-    if (box.innerText != "" ) return;
+    if (box.innerText != "") return;
     box.innerText = currentPlayer;
     currentPlayer == "X" ? currentPlayer = "O" : currentPlayer = "X";
     checkGameBoard()
@@ -22,12 +22,11 @@ function checkGameBoard() {
         third.innerText
         if (first == "") continue
         if (first == second && first == third) {
-            let winner=document.getElementById("winner")
-            winner.style.display="block"
+            let winner = document.getElementById("winner")
+            winner.style.display = "block"
         }
-     
-       
     }
+
     for (let i = 0; i <= 2; i++) {
         let first = document.getElementById(i + "_0").innerText
         let second = document.getElementById(i + "_1").innerText
@@ -35,19 +34,22 @@ function checkGameBoard() {
         third.innerText
         if (first == "") continue
         if (first == second && first == third) {
-            winner.style.display="block"
+            winner.style.display = "block"
         }
     }
+
     let firstD1 = document.getElementById("0_0").innerText
     let secondD1 = document.getElementById("1_1").innerText
     let thirdD1 = document.getElementById("2_2").innerText
     if (firstD1 != "" && firstD1 == secondD1 && firstD1 == thirdD1) {
-        winner.style.display="block"
+        winner.style.display = "block"
     }
+    
     let firstD2 = document.getElementById("0_2").innerText
     let secondD2 = document.getElementById("1_1").innerText
     let thirdD2 = document.getElementById("2_0").innerText
     if (firstD2 != "" && firstD2 == secondD2 && firstD2 == thirdD2) {
-        winner.style.display="block"
+        winner.style.display = "block"
     }
+
 }
