1b2ac5a85e0c1ddbfb05b0aec58c716e12918301
NataManatu <vip.aleksa78@mail.ru>
2022-08-05 21:56:55 +0300

    TicTac Toe

--- script.js
+++ script.js
@@ -19,19 +19,18 @@ function checkGameBoard() {
         let first = document.getElementById("0_" + i).innerText
         let second = document.getElementById("1_" + i).innerText
         let third = document.getElementById("2_" + i).innerText
-        third.innerText
         if (first == "") continue
         if (first == second && first == third) {
             let winner = document.getElementById("winner")
             winner.style.display = "block"
         }
+       
     }
 
     for (let i = 0; i <= 2; i++) {
         let first = document.getElementById(i + "_0").innerText
         let second = document.getElementById(i + "_1").innerText
         let third = document.getElementById(i + "_2").innerText
-        third.innerText
         if (first == "") continue
         if (first == second && first == third) {
             winner.style.display = "block"
@@ -44,7 +43,7 @@ function checkGameBoard() {
     if (firstD1 != "" && firstD1 == secondD1 && firstD1 == thirdD1) {
         winner.style.display = "block"
     }
-    
+
     let firstD2 = document.getElementById("0_2").innerText
     let secondD2 = document.getElementById("1_1").innerText
     let thirdD2 = document.getElementById("2_0").innerText
